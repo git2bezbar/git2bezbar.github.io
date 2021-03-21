a5901e2e01bb5c7e7d39cc88c52d74d2db1ef1d0
A3DEM <ademduran6888@gmail.com>
2021-03-21 13:55:15 +0100

    fix

--- mocha_cafe_numendo/service-worker.js
+++ mocha_cafe_numendo/service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener("install", function (event) {
         caches.open(version + 'fundamentals')
             .then(function (cache) {
                 return cache.addAll([
-                    '/',
+                    '/mocha_cafe_numendo',
                     'index.html',
                     'manifest.json'
                 ]);
