94f870eaf87906a2801d7ac38d6fe0e3e517cfe0
A3DEM <ademduran6888@gmail.com>
2021-06-16 20:54:48 +0200

    https

--- app_meteo/js/script.js
+++ app_meteo/js/script.js
@@ -6,12 +6,12 @@ function main() {
             const ip = json.ip;
             console.log(ip);
 
-            fetch('http://freegeoip.net/json/' + ip)
+            fetch('https://freegeoip.net/json/' + ip)
                 .then(resultat => resultat.json())
                 .then(json => {
                     const ville = json.city;
 
-                    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric')
+                    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric')
                         .then(resultat => resultat.json())
                         .then(json => {
                             console.log(json);
