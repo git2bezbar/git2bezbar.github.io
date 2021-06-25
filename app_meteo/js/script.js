d126d357d85271412943513abb25ae051ffdbac4
A3DEM <ademduran6888@gmail.com>
2021-06-25 16:32:27 +0200

    aaa

--- app_meteo/js/script.js
+++ app_meteo/js/script.js
@@ -2,22 +2,23 @@ function main() {
 
     fetch('https://api.ipify.org?format=json')
         .then(resultat => resultat.json())
-        .then(json => {
-            const ip = json.ip;
-            console.log(ip);
+        .then(console.log(resultat.json()));
+    /* .then(json => {
+        const ip = json.ip;
+        console.log(ip);
 
-            /* fetch('http://freegeoip.net/json/' + ip)
-                .then(resultat => resultat.json())
-                .then(json => {
-                    const ville = json.city;
+        fetch('http://freegeoip.net/json/' + ip)
+            .then(resultat => resultat.json())
+            .then(json => {
+                const ville = json.city;
 
-                    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=4a337615020c6ec12925a53f61106167&lang=fr&units=metric')
-                        .then(resultat => resultat.json())
-                        .then(json => {
-                            console.log(json);
-                        })
-                }) */
-        })
+                fetch('http://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=4a337615020c6ec12925a53f61106167&lang=fr&units=metric')
+                    .then(resultat => resultat.json())
+                    .then(json => {
+                        console.log(json);
+                    })
+            })
+    }) */
 }
 
 main();
